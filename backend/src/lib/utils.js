0ab270a1784411cccf6933fbe3f3278e5ba72b39
surajsingh9389 <sk9389623488@gmail.com>
2025-07-02 22:48:41 +0530

    jwt fix

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken= (userId, res)=>{
     res.cookie("jwt", token, {
         maxAge: 7*24*60*60*1000,
         httpOnly: true, //prevent XSS attack
-        sameSite: "strict", // CSRF attacks 
+        sameSite: "None", // CSRF attacks 
         secure: process.env.NODE_ENV !== "development"
     });
 
