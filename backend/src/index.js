512f37f25968390bc1987dc5a53395ddd9fdd414
surajsingh9389 <sk9389623488@gmail.com>
2025-07-02 13:51:14 +0530

    vercel add

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,13 +25,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if(process.env.NODE_ENV==="production"){
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get(/(.*)/, (req, res) =>{
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
-}
+//   app.get(/(.*)/, (req, res) =>{
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   })
+// }
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
