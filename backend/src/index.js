36c347256141d18b00257ec7ff206817d9cf1802
surajsingh9389 <sk9389623488@gmail.com>
2025-07-02 21:58:49 +0530

    check

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,10 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.get("/", (req, res) => {
+  res.send("✅ Backend is up and running!");
+});
+
 // if(process.env.NODE_ENV==="production"){
 //   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
